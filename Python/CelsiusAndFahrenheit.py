bec38bc81877674a49bb06ef9771da61c54b78dc
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-09-27 16:53:20 -0400

    Fixed multiplication bug and added tests

--- Python/CelsiusAndFahrenheit.py
+++ Python/CelsiusAndFahrenheit.py
@@ -15,6 +15,18 @@ def celsius_to_fahrenheit(temp_c: float) -> float:
     conversion_factor = (9/5) 
     offset = 32 # Offset as 0 degrees C is 32 degrees F
 
-    temp_f = (temp_c × conversion_factor) + offset
+    temp_f = (temp_c * conversion_factor) + offset
 
     return temp_f
+
+
+def test_celsius_to_fahrenheit():
+    """
+    Tests the celsius to fahrenheit function. 
+    """
+    assert celsius_to_fahrenheit(0) == 32.0 # Freezing for water
+    assert celsius_to_fahrenheit(100) == 212.0 # Boiling for water
+
+
+if __name__=="__main__":
+    test_celsius_to_fahrenheit()
